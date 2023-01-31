0042473c356049fac21846dc36cee5d756dd872f
Emerald <twac@st-andrews.ac.uk>
2023-01-31 13:28:32 +0000

    refactor: example shoul use point in dataset

--- example/src/main.rs
+++ example/src/main.rs
@@ -15,8 +15,13 @@ fn main() {
 
     let bitpart = BitPartBuilder::new(nasa.clone()).build();
 
-    let query = Euclidean::new(Nasa([0.0; 20]));
-    let threshold = 0.5;
+    // Line 319 in nasa.ascii
+    let query = Euclidean::new(Nasa([
+        0.00722561, 0.0599118, 0.0165916, 0.121793, 0.0404137, 0.297534, 0.979138, -0.792623,
+        0.242515, 0.162952, -0.209939, 0.0275739, -0.16217, -0.0176906, -0.0309458, 0.0530525,
+        -0.437606, 0.00760368, -0.153654, 0.0296254,
+    ]));
+    let threshold = 1.0;
 
     let res = bitpart.range_search(query.clone(), threshold);
     println!("{} points returned", res.len());
