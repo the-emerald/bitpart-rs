2d467ebc16e387c22ab06439dfec2a92f0cf696f
Emerald <twac@st-andrews.ac.uk>
2023-04-08 06:02:05 +0100

    fix: flat range should be -0.5 to 0.5

--- make-data.sh
+++ make-data.sh
@@ -4,5 +4,9 @@ SEED=3132448493
 cargo run --release -p generators -- -d 20 -p 1000000 -s ${SEED} -o "data/output.ascii" normal --mean 0 --std-dev 1.0
 
 # Make 100k flat
-cargo run --release -p generators -- -d 20 -p 100000 -s ${SEED} -o "data/100k_flat.ascii" flat --low "-1.0" --high "1.0"
-cargo run --release -p nearest-neighbours -- -i "data/100k_flat.ascii" -n 10 -o "data/100k_flat.json"
+cargo run --release -p generators -- -d 20 -p 100000 -s ${SEED} -o "data/100k_flat.ascii" flat --low "-0.5" --high "0.5"
+cargo run --release -p nearest-neighbours -- -i "data/100k_flat.ascii" -n 10 -o "data/100k_flat.json" -p 1000
+
+# # Make 150M flat (on-disk)
+# cargo run --release -p generators -- -d 20 -p 15000000 -s ${SEED} -o "data/150M_flat.ascii" flat --low "-0.5" --high "0.5"
+# cargo run --release -p nearest-neighbours -- -i "data/150M_flat.ascii" -n 10 -o "data/150M_flat.json" -p 1000
