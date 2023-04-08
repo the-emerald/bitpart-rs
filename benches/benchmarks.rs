b6cfb0619f983937f46ad957f094798fd434ec22
Emerald <twac@st-andrews.ac.uk>
2023-04-08 07:52:41 +0100

    refactor: make 10d to 20d data

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -284,7 +284,7 @@ pub fn nn_query_inner<T>(
 }
 
 pub fn nn_query(c: &mut Criterion) {
-    let points = parse(&fs::read_to_string("data/100k_flat.ascii").unwrap())
+    let points = parse(&fs::read_to_string("data/100k_d20_flat.ascii").unwrap())
         .unwrap()
         .1
          .1
@@ -293,7 +293,7 @@ pub fn nn_query(c: &mut Criterion) {
         .collect::<Vec<_>>();
 
     let thresholds = serde_json::from_str::<Vec<Vec<(usize, f64)>>>(
-        &fs::read_to_string("data/100k_flat.json").unwrap(),
+        &fs::read_to_string("data/100k_d20_flat.json").unwrap(),
     )
     .unwrap()
     .into_iter()
@@ -304,7 +304,7 @@ pub fn nn_query(c: &mut Criterion) {
 
     nn_query_inner(
         c,
-        "100k_flat".to_string(),
+        "100k_d20_flat".to_string(),
         1000,
         builder,
         points,
--- make-data.sh
+++ make-data.sh
@@ -1,11 +1,20 @@
 SEED=3132448493
 
-# Make 1M points (synthetic)
-cargo run --release -p generators -- -d 20 -p 1000000 -s ${SEED} -o "data/output.ascii" normal --mean 0 --std-dev 1.0
+cargo build --release -p generators
+cargo build --release -p nearest-neighbours
 
-# Make 100k flat
-cargo run --release -p generators -- -d 20 -p 100000 -s ${SEED} -o "data/100k_flat.ascii" flat --low "-0.5" --high "0.5"
-cargo run --release -p nearest-neighbours -- -i "data/100k_flat.ascii" -n 10 -o "data/100k_flat.json" -p 1000
+# Make 1M points (synthetic)
+echo "synthetic"
+cargo run -q --release -p generators -- -d 20 -p 1000000 -s ${SEED} -o "data/output.ascii" normal --mean 0 --std-dev 1.0
+
+# 100k, differing dimensions
+for DIMS in {10..20}
+do
+    echo "100k, d${DIMS}"
+    # Make 100k flat
+    cargo run -q --release -p generators -- -d ${DIMS} -p 100000 -s ${SEED} -o "data/100k_d${DIMS}_flat.ascii" flat --low "-0.5" --high "0.5"
+    cargo run -q --release -p nearest-neighbours -- -i "data/100k_d${DIMS}_flat.ascii" -n 10 -o "data/100k_d${DIMS}_flat.json" -p 1000
+done
 
 # # Make 150M flat (on-disk)
 # cargo run --release -p generators -- -d 20 -p 15000000 -s ${SEED} -o "data/150M_flat.ascii" flat --low "-0.5" --high "0.5"
