9762b77d22036cca1e49f7fe234d14b033f7d4ca
Emerald <twac@st-andrews.ac.uk>
2023-05-04 14:15:07 +0100

    Revert "refactor: remove parallel from bitvec deser"
    This reverts commit 25b12b1cfcc9b32a6d50dd6633d8f533ddf34b26.

--- src/on_disk.rs
+++ src/on_disk.rs
@@ -56,12 +56,12 @@ where
             .partition_map(|x| x);
 
         let ins = ins
-            .into_iter()
+            .into_par_iter()
             .map(|idx| bincode::deserialize::<BitVec>(self.bitset.get(idx).unwrap()).unwrap())
             .collect::<Vec<_>>();
 
         let outs = outs
-            .into_iter()
+            .into_par_iter()
             .map(|idx| bincode::deserialize::<BitVec>(self.bitset.get(idx).unwrap()).unwrap())
             .collect::<Vec<_>>();
 
